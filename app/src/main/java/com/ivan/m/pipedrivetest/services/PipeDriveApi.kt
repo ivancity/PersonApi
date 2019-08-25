39ce9eb16e02d102e386bfb2001fd73f7ea36d11
Ivan Morris <>
2019-08-25 18:58:05 -0500

    code cleanup

--- app/src/main/java/com/ivan/m/pipedrivetest/services/PipeDriveApi.kt
+++ app/src/main/java/com/ivan/m/pipedrivetest/services/PipeDriveApi.kt
@@ -1,7 +1,6 @@
 package com.ivan.m.pipedrivetest.services
 
 import com.ivan.m.pipedrivetest.models.PersonsResponse
-import kotlinx.coroutines.Deferred
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
