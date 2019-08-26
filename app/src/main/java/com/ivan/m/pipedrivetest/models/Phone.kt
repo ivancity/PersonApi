31994c52e3830e07671593675b2db52ef4448717
Ivan Morris <>
2019-08-26 01:27:05 -0500

    adding an autogenerated id for the phone table

--- app/src/main/java/com/ivan/m/pipedrivetest/models/Phone.kt
+++ app/src/main/java/com/ivan/m/pipedrivetest/models/Phone.kt
@@ -1,9 +1,11 @@
 package com.ivan.m.pipedrivetest.models
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity
 data class Phone(
+    @PrimaryKey(autoGenerate = true) var id: Int?,
     var label: String?,
     var value: String?,
     var ownerId: Int?)
